91ac937b5d73a9b35670dbabae8db70600f2f0eb
Marcus Bakke <marcusabakke@gmail.com>
2024-05-29 22:08:29 -0700

    Add view helper!

--- make_html.py
+++ make_html.py
@@ -4,7 +4,9 @@ import ipdb
 with open(os.path.join('utils', 'template.html'), 'r') as f:
     content = f.read()
 with open(os.path.join('utils', 'femview.mjs'), 'r') as f:
-    code = f.read()
+    femview = f.read()
+with open(os.path.join('utils', 'ViewHelper.js'), 'r') as f:
+    viewhelper = f.read()
 
 with open(os.path.join('utils', 'nodes.txt'), 'r') as f:
     nodes = f.read()
@@ -13,9 +15,10 @@ with open(os.path.join('utils', 'faces.txt'), 'r') as f:
 with open(os.path.join('utils', 'lines.txt'), 'r') as f:
     lines = f.read()
 
-code = code.replace("REPLACE_NODES", nodes)
-code = code.replace("REPLACE_FACES", faces)
-code = code.replace("REPLACE_LINES", lines)
-content = content.replace("REPLACE_ME", code)
+femview = femview.replace("REPLACE_NODES", nodes)
+femview = femview.replace("REPLACE_FACES", faces)
+femview = femview.replace("REPLACE_LINES", lines)
+content = content.replace("REPLACE_FEMVIEW", femview)
+content = content.replace("REPLACE_VIEWHELPER", viewhelper)
 with open('index.html', 'w') as f:
     f.write(content)
